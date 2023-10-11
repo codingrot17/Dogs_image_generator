1097842aeb6ef10e305c0c0826a76c7fe095e1d4
Ademola Emmanuel <141020350+codingrot17@users.noreply.github.com>
2023-10-11 11:25:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ function apeImage(){
     const data = res.json();
     return data;
   }).then((dataObj) => {
+    // for dog Image 
     const imageEl = document.getElementById('images');
     imageEl.src = dataObj.message;
-    // console.log(dataObj.message);
+    // for dog name
+    let dogers = dataObj.message;
+    dogers = dogers.split('/')
+    const dogName = dogers[dogers.length-2];
+    const nameOfDog = document.getElementById('name');
+    nameOfDog.textContent = dogName;
   })
 }
 
